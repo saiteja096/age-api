097ac2fcad97057c38334c57563c68386eb761aa
saiteja <54365343+saiteja096@users.noreply.github.com>
2021-12-03 20:04:34 +0530

    Update emp.py

--- resources/emp.py
+++ resources/emp.py
@@ -35,3 +35,64 @@ class Profile(Resource):
             return {"message":"There was an error inserting into user table."+str(e)+""},500
 
         return {"message":"Successfully Inserted."},201
+
+def inf_grow(i):
+    i = i + (6/100)*i
+    return i
+
+class Age(Resource):
+    def get(self):
+        try:
+            return query("""SELECT * FROM user""")
+        except:
+            return {"message":"There was an error connecting to user table."},500
+
+    def post(self):
+        
+        parser=reqparse.RequestParser()
+        parser.add_argument('name',type=str,required=True,help="name cannot be left blank!")
+        parser.add_argument('gender',type=str,required=True,help="gender cannot be left blank!")
+        parser.add_argument('age',type=int,required=True,help="age cannot be left blank!")
+        parser.add_argument('pref_r_age',type=int,required=True,help="pref_r_age cannot be left blank!")
+        parser.add_argument('income',type=int,required=True,help="income cannot be left blank!")
+        parser.add_argument('incomerate',type=int,required=True,help="incomerate cannot be left blank!")
+        parser.add_argument('expenditure',type=int,required=True,help="expenditure cannot be left blank!")
+        parser.add_argument('savingsrate',type=int,required=True,help="savingsrate cannot be left blank!")
+        data=parser.parse_args()
+        try:
+            income = data['income']
+            expense = data['expenditure']
+            saving_rate = data['savingsrate']
+            inflation = data['incomerate']
+            growth = data['incomerate']
+            age = data['age']
+            total_saving = 0
+
+            saving = income - expense
+            total_saving = total_saving + saving
+
+
+            for age in range(age+1,86):
+                print("Age = "+str(age))
+                income = inf_grow(income)
+                expense = inf_grow(expense)
+                rate_return = (10/100)*total_saving
+                saving = income - expense
+                total_saving = int(total_saving + saving + rate_return)
+                print("cashflow = " + str(total_saving))
+
+                n_total_saving = total_saving
+                n_rate_return = rate_return
+                n_expense = expense
+
+                for j in range(age+1,86):
+                    n_total_saving = int(n_total_saving - n_expense + n_rate_return)
+                    n_rate_return = (10/100)*n_total_saving
+                    n_expense = inf_grow(n_expense)
+                    if n_total_saving < 0:
+                        print("If retired at age = " + str(age) + " Savings completed at the age = " + str(j))
+                        return {"message":age},201
+        except Exception as e:
+            return {"message":"There was an error inserting into user table."+str(e)+""},500
+
+        return {"message":"Successfully Inserted."},201
