689ed22d2eeb1b4a86cf80070f8e3a6fc9b04581
saiteja <54365343+saiteja096@users.noreply.github.com>
2021-12-03 20:55:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 from resources.emp import Profile,Age
+from flask_cors import CORS
 
 app=Flask(__name__)
 
 app.config['PROPAGATE_EXCEPTIONS']=True
 api=Api(app)
+CORS(app)
 api.add_resource(Profile,'/profile')
 
 api.add_resource(Age,'/age')
